1c05a6778f172b1859ea089d1af47c147f6635ca
Ethan Staggs <78055596+Ethan-Staggs@users.noreply.github.com>
2024-10-10 18:41:33 -0400

    Walk testing

--- Enemies/skeleton_mage.gd
+++ Enemies/skeleton_mage.gd
@@ -1,22 +1,16 @@
 extends CharacterBody3D
 @onready var skeleton = $AnimationPlayer
 @onready var nav = $NavigationAgent3D
-@onready var player = $Rig/Skeleton3D
 
-const SPEED = 10.0
+const SPEED = 1.0
 
 var accel = 10
 
 
 func _physics_process(delta: float) -> void:
 	skeleton.play("Walking_A")
-	var direction = Vector3()
-	
-	nav.target_position = player.global_position
-	direction = nav.get_next_path_position() - global_position
-	direction = direction.normalized()
-	
-	velocity = velocity.lerp(direction * SPEED, accel * delta)
-	
+	velocity = Vector3.BACK * SPEED
 	move_and_slide()
+	
+
 	
