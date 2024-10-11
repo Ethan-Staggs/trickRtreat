aaa92dd5ae6ff2a4e27dc484fc2e449ec36aa518
Ethan Staggs <78055596+Ethan-Staggs@users.noreply.github.com>
2024-10-11 08:32:43 -0400

    Enemy chase code

--- Enemies/skeleton_mage.gd
+++ Enemies/skeleton_mage.gd
@@ -4,13 +4,22 @@ extends CharacterBody3D
 
 const SPEED = 1.0
 
+var radius = 3
 var accel = 10
 
 
+
 func _physics_process(delta: float) -> void:
+	var currentLocation = global_transform.origin
+	var nextLocation = nav.get_next_path_position()
+	var newVelocity = (nextLocation - currentLocation).normalized() * SPEED
 	skeleton.play("Walking_A")
-	velocity = Vector3.BACK * SPEED
+	velocity = newVelocity
+	print(global_position)
 	move_and_slide()
+	
+func updateTargetLocation(targetLocation):
+	nav.set_target_position(targetLocation)
 	
 
 	
