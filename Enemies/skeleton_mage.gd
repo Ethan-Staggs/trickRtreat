45b1a997e432514809fb28af77e603616ad0719b
Ethan Staggs <78055596+Ethan-Staggs@users.noreply.github.com>
2024-10-11 09:09:44 -0400

    Chase code in enemy script

--- Enemies/skeleton_mage.gd
+++ Enemies/skeleton_mage.gd
@@ -2,20 +2,18 @@ extends CharacterBody3D
 @onready var skeleton = $AnimationPlayer
 @onready var nav = $NavigationAgent3D
 
-const SPEED = 1.0
+const SPEED = 5.0
 
 var radius = 3
 var accel = 10
 
-
-
 func _physics_process(delta: float) -> void:
+	skeleton.play("Running_A")
 	var currentLocation = global_transform.origin
 	var nextLocation = nav.get_next_path_position()
 	var newVelocity = (nextLocation - currentLocation).normalized() * SPEED
-	skeleton.play("Walking_A")
 	velocity = newVelocity
-	print(global_position)
+	print(newVelocity)
 	move_and_slide()
 	
 func updateTargetLocation(targetLocation):
--- main.gd
+++ main.gd
@@ -2,13 +2,5 @@ extends Node3D
 
 @onready var player = $Player
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
 func _physics_process(delta):
 	get_tree().call_group("enemies", "updateTargetLocation", player.global_transform.origin)
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
