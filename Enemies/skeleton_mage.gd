bfe63777c2765e8e8ea7a8322b3db7548b9cde5a
raptor0999 <raptordev@proton.me>
2024-10-11 14:38:24 -0400

    Adds a path3d and pathfollow3d to enemy in the main scene as an example

--- Enemies/skeleton_mage.gd
+++ Enemies/skeleton_mage.gd
@@ -13,18 +13,19 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 	
 	# find your target to follow, in this case the player
-	followTarget = get_tree().get_first_node_in_group("player")
+	#followTarget = get_tree().get_first_node_in_group("player")
 	
 	# find which direction you need to go to followTarget
-	var direction = position.direction_to(followTarget.global_position)
+	#var direction = position.direction_to(followTarget.global_position)
 	
 	# rotate toward the target
-	look_at(followTarget.global_position)
-	self.rotate_object_local(Vector3.UP, PI)
+	#if is_instance_valid(followTarget):
+		#look_at(followTarget.global_position)
+		#self.rotate_object_local(Vector3.UP, PI)
 	
-	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
-		skeleton.play("Walking_A")
+	#if direction:
+		#velocity.x = direction.x * SPEED
+		#velocity.z = direction.z * SPEED
+	skeleton.play("Walking_A")
 	
 	move_and_slide()
