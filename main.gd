c2f6ff43c8c1c66ee3d43c18c5b4a10d7ea3ab0c
Ethan Staggs <78055596+Ethan-Staggs@users.noreply.github.com>
2024-10-15 14:37:43 -0400

    Added more enemies, candy buckets label and timer

--- main.gd
+++ main.gd
@@ -1,9 +1,18 @@
 extends Node3D
 @onready var backgroundMusic = $BackgroundMusic
 @onready var player = $Player
+@onready var label = $Label
+@onready var timer = $Timer
+
+
 
 func _ready() -> void:
 	backgroundMusic.autoplay
+	timer.start(60)
+	label.set("theme_override_fonts/font", load("res://Assets/Fonts/Haedus.ttf"))
+	
+	
 
 func _physics_process(delta):
+	label.set_text(str(timer.get_time_left()).pad_decimals(1))
 	get_tree().call_group("enemies", "updateTargetLocation", player.global_transform.origin)
