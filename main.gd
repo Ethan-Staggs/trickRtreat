677141ed8573aa151c3d607bcb4852bd9b3dde6b
Ethan Staggs <78055596+Ethan-Staggs@users.noreply.github.com>
2024-10-15 11:43:12 -0400

    Added 2 new enemies with paths and follow paths. Added background music

--- main.gd
+++ main.gd
@@ -1,6 +1,9 @@
 extends Node3D
-
+@onready var backgroundMusic = $BackgroundMusic
 @onready var player = $Player
+
+func _ready() -> void:
+	backgroundMusic.autoplay
 
 func _physics_process(delta):
 	get_tree().call_group("enemies", "updateTargetLocation", player.global_transform.origin)
