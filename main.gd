4c7b3c25275d3681c37215cad9ebe7a592d8199f
Ethan Staggs <78055596+Ethan-Staggs@users.noreply.github.com>
2024-10-15 15:17:49 -0400

    Added health bar

--- main.gd
+++ main.gd
@@ -3,10 +3,12 @@ extends Node3D
 @onready var player = $Player
 @onready var label = $Label
 @onready var timer = $Timer
+@onready var healthBar = $HealthBar
 
 
 
 func _ready() -> void:
+	healthBar.show_percentage = true
 	backgroundMusic.autoplay
 	timer.start(60)
 	label.set("theme_override_fonts/font", load("res://Assets/Fonts/Haedus.ttf"))
